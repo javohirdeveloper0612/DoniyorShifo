57cf6c95ec03b063e8160c81f378c654826c67ec
Firdavs <firdavsprogramer@gmail.com>
2023-01-21 10:19:22 -0800

    commmitted

--- src/main/java/com/example/config/SwaggerConfig.java
+++ src/main/java/com/example/config/SwaggerConfig.java
@@ -1,6 +1,4 @@
 package com.example.config;
-
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -10,5 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(info = @Info(title = "Doniyorshifo.uz APi documentation", version = "1.0", description = ""))
 @ConditionalOnProperty(value = "springfox.documentation.enabled", havingValue = "true", matchIfMissing = true)
 public class SwaggerConfig {
+
+
 }
 
--- src/main/java/com/example/controller/AttachController.java
+++ src/main/java/com/example/controller/AttachController.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
